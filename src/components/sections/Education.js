23194188d127b0d27cd92df55a34b470638df220
Leon Gregori <mail@leongregori.com>
2020-07-10 12:41:46 +0200

    Update Education.js

--- src/components/sections/Education.js
+++ src/components/sections/Education.js
@@ -1,27 +1,13 @@
 import React from "react"
 
 import Timeline from "../elements/EducationTimeline"
-import CertificationTimeline from "../elements/CertificationTimeline"
 
 function Education() {
     return(
         <section className="section" id="education">
             <div className="container">
                 <h1 className="title">Education</h1>
-                <div className="columns">
-                    <div className="column is-6">
-                        <div className="has-text-centered">
-                            <h2 className="title is-5">School & University</h2>
-                        </div>
-                        <Timeline/>
-                    </div>
-                    <div className="column is-6">
-                        <div className="has-text-centered">
-                            <h2 className="title is-5">Certifications</h2>
-                        </div>
-                        <CertificationTimeline/>
-                    </div>
-                </div>
+                <Timeline />
             </div>
         </section>
     )
