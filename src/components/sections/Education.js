2fb9b19c0023cebd656826bfa88d523b783bf295
Leon Gregori <mail@leongregori.com>
2020-07-10 11:17:57 +0200

    Update Education.js

--- src/components/sections/Education.js
+++ src/components/sections/Education.js
@@ -7,10 +7,30 @@ function Education() {
         <section className="section" id="education">
             <div className="container">
                 <h1 className="title">Education</h1>
-                <Timeline />
+                <div className="columns">
+                    <div className="column is-6">
+                        <div className="has-text-centered">
+                            <span className="icon has-text-link">
+                                <i className="fas fa-3x fa-cogs"></i>
+                            </span>
+                            <h2 className="title is-5">Professional Education</h2>
+                        </div>
+                        <Timeline/>
+                    </div>
+                    <div className="column is-6">
+                        <div className="has-text-centered">
+                            <span className="icon has-text-link">
+                                <i className="fas fa-3x fa-laptop-code"></i>
+                            </span>
+                            <h2 className="title is-5">Education</h2>
+                        </div>
+                        <Timeline/>
+                    </div>
+                </div>
             </div>
         </section>
     )
 }
 
 export default Education
+
