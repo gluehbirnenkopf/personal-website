9dcb1bb5f23ca4e63619a2d5134c7ac52c98d56f
Leon Gregori <mail@leongregori.com>
2020-07-10 12:43:13 +0200

    Update Certification.js

--- src/components/sections/Certification.js
+++ src/components/sections/Certification.js
@@ -1,1 +1,15 @@
+import React from "react"
 import CertificationTimeline from "../elements/CertificationTimeline"
+
+function Certifications() {
+    return(
+        <section className="section" id="experience">
+            <div className="container">
+                <h1 className="title">Certifications</h1>
+                <CertificationTimeline />
+            </div>
+        </section>
+    )
+}
+
+export default Certifications
