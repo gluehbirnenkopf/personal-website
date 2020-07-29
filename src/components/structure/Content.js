eb54c38abe7f2761ffe53cb6961f2d4671541840
Leon Gregori <mail@leongregori.com>
2020-07-29 20:18:13 +0200

    Commenting out Articles as currently not used.

--- src/components/structure/Content.js
+++ src/components/structure/Content.js
@@ -14,7 +14,7 @@ function Content() {
             <Experience />
             <Education />
             <Certifications />
-            <Articles />
+            //<Articles />
         </main>
     )
 }
